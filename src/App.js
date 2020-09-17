9c0e2a6212a9c1fa661489913bb0225a30088c47
David Cha <littlesadtea@gmail.com>
2020-09-17 14:25:03 -0400

    practicing hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './App.css'
+
 
 function App() {
+
+  const [state, setState] = useState( { count: 24, clicked: 0 } );
+  const count = state.count
+  const clicked = state.clicked
+
+  const [newCount, setNewCount] = useState(2)
+  const [newClicked, setNewClicked] = useState(0)
+
+  function decreaseCount() {
+    setState( prevState => {
+      return { count: prevState.count - 1, clicked: prevState.clicked + 1 }
+    })
+  }
+
+  function increaseCount() {
+    setState( prevState => {
+      return { count: prevState.count + 2, clicked: prevState.clicked + 1 }
+    })
+  }
+
+  function newDecreaseCount() {
+    setNewCount(prevNewCount => prevNewCount - 1)
+    setNewClicked(prevNewClicked => prevNewClicked + 1)
+  }
+
+  function newIncreaseCount() {
+    setNewCount(prevNewCount => prevNewCount + 2)
+    setNewClicked(prevNewClicked => prevNewClicked + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={decreaseCount}>-</button>
+      <span>{count}</span>
+      <button onClick={increaseCount}>+</button>
+      <span>clicked: {clicked}</span>
+
+      <hr/>
+
+      <button onClick={newDecreaseCount}>-</button>
+      <span>{newCount}</span>
+      <button onClick={newIncreaseCount}>+</button>
+      <span>newClicked: {newClicked}</span>  
     </div>
   );
 }
